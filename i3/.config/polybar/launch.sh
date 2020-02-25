915c14852449b3c73e23e1693ef4752aa982ca51
Jon Turinese <jturinese@d3engineering.com>
2020-02-25 17:55:43 -0500

    modified polybar

---
+++ i3/.config/polybar/launch.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+# If all your bars have ipc enabled, you can also use
+# polybar-msg cmd quit
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+polybar top -r &
+
+echo "Bars launched..."
